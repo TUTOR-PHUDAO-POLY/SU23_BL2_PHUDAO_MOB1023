0911168b1768ab1023c4f866fe3828aef50dfddd
HangNT169 <HangNT169@fpt.edu.vn>
2023-07-31 02:44:28 +0700

    Init project

---
+++ src/su23_bl2_phudao_mob1023/SU23_BL2_PHUDAO_MOB1023.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package su23_bl2_phudao_mob1023;
+
+/**
+ *
+ * @author hangnt
+ */
+public class SU23_BL2_PHUDAO_MOB1023 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
